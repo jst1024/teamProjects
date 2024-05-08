0449b75c5b9bed83fb2f696733e57738434719cd
shykkoma <shykkoma@gmail.com>
2024-05-08 15:10:11 +0900

    Fix member error fix

--- teamproject/src/org/busan/ctrl/sharetrip/StInsCtrl.java
+++ teamproject/src/org/busan/ctrl/sharetrip/StInsCtrl.java
@@ -10,9 +10,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.tomcat.util.http.fileupload.FileItem;
-import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
-import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
 
 @WebServlet("/StIns.do")
 public class StInsCtrl extends HttpServlet {
